3fe373a24a36ccb0a3624820f0386e816fe1195d
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-01-08 16:18:40 -0300

    Implementa LogIn de usuário

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+
+use function PHPSTORM_META\type;
 
 class AuthController extends Controller
 {
@@ -26,8 +28,33 @@ class AuthController extends Controller
         $user = User::create($userData);
 
         return response()->json([
-            'message'   => 'Successfully registered',
+            'message'   => 'Successfully registered!',
             'user'      => $user
         ], Response::HTTP_CREATED);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+        $token = auth()->attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'error' => 'Unauthorized!'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response()->json([
+            'token' => $token,
+            'type' => 'Bearer',
+        ], Response::HTTP_OK);
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json([
+            'message' => 'Logged out successfully!'
+        ], Response::HTTP_OK);
     }
 }
